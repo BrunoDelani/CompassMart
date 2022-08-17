e4edd074413dc55d8e39c5f793b1f461f52c1cac
BrunoDelani <br.delani@gmail.com>
2022-08-17 17:38:55 -0400

    feat: route get by low stock

--- src/api/services/product-service.ts
+++ src/api/services/product-service.ts
@@ -5,10 +5,20 @@ import ProductsNotFound from '../../errors/product/products-not-found';
 import { IProduct, IProductQuery, IProductResponse } from '../models/interfaces/product-interface';
 import productRepository from '../repositories/product-repository';
 import { ObjectId, PaginateResult } from 'mongoose';
+import { IPaginate } from '../models/interfaces/paginate-interface';
 
 class ProductService {
-  async findProducts (query: IProductQuery): Promise<PaginateResult<IProductResponse>> {
+  async findProducts (query: IPaginate): Promise<PaginateResult<IProductResponse>> {
     const results = await productRepository.find(query);
+    if ((query.page && results.offsets) && (query.page > results.offsets)) throw new PageNotFound();
+    if (query.page && query.page <= 0) throw new PageNotFound();
+    if (query.limit && query.limit <= 0) throw new PageNotFound();
+    if (!results.total) throw new ProductsNotFound();
+    return results;
+  }
+
+  async findProductsLowStock (query: IProductQuery): Promise<PaginateResult<IProductResponse>> {
+    const results = await productRepository.findByLowStock(query);
     if ((query.page && results.offsets) && (query.page > results.offsets)) throw new PageNotFound();
     if (query.page && query.page <= 0) throw new PageNotFound();
     if (query.limit && query.limit <= 0) throw new PageNotFound();
@@ -24,7 +34,7 @@ class ProductService {
 
   async createProduct (payload: IProduct): Promise<IProductResponse|null> {
     if (await productRepository.findByBarCode(payload.bar_codes)) throw new ProductBarcodesExists();
-    payload.stock_control_enabled = true;
+    if (payload.qtd_stock > 0) payload.stock_control_enabled = true;
     return await productRepository.create(payload);
   }
 
