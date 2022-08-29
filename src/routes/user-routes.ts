d78ae8517e5e48cb57294ddacd83d39e59a65f18
BrunoDelani <br.delani@gmail.com>
2022-08-29 18:18:43 -0400

    feat: error handling user id validation to delete

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import userController from '../api/controllers/user-controller';
 import createUserValidation from '../api/validations/user/create-user-validation';
+import idUserValidation from '../api/validations/user/id-user-validation';
 
 const router = Router();
 const urlBaseRoute = '/api/v1';
 router
   .get(`${urlBaseRoute}/user`, userController.findUser)
   .post(`${urlBaseRoute}/user`, createUserValidation, userController.createUser)
-  .delete(`${urlBaseRoute}/user/:id`, userController.deleteUser)
+  .delete(`${urlBaseRoute}/user/:id`, idUserValidation, userController.deleteUser)
 ;
 
 export default router;
