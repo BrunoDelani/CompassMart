58f083807cd901ecd2b73d55c24bb99f25fe83ce
BrunoDelani <br.delani@gmail.com>
2022-08-29 16:13:38 -0400

    feat: validation create user

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import userController from '../api/controllers/user-controller';
+import createUserValidation from '../api/validations/user/create-user-validation';
 
 const router = Router();
 const urlBaseRoute = '/api/v1';
 router
-  .post(`${urlBaseRoute}/user`, userController.createUser);
+  .post(`${urlBaseRoute}/user`, createUserValidation, userController.createUser);
 
 export default router;
