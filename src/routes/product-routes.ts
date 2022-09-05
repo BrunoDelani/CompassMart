4a2aa9733b29779644edcfd7d22faaf953cbf692
BrunoDelani <br.delani@gmail.com>
2022-09-05 07:39:52 -0400

    feat: user authentication middleware in route mapper

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -11,14 +11,14 @@ const multerConfig = multer();
 const router = Router();
 const urlBaseRoute = '/api/v1/product';
 router
-  .get(`${urlBaseRoute}/marketplace/:id`, IdProductValidation, productController.mapperProductByID)
   .get(`${urlBaseRoute}`, authMiddleware, productController.findProduct)
-  .get(`${urlBaseRoute}/low_stock`, authMiddleware, productController.findProductLowStock)
-  .get(`${urlBaseRoute}/:id`, authMiddleware, IdProductValidation, productController.findProductByID)
   .post(`${urlBaseRoute}`, authMiddleware, createProductValidation, productController.createProduct)
-  .post(`${urlBaseRoute}/csv`, authMiddleware, multerConfig.single('file'), productController.createProductCSV)
+  .get(`${urlBaseRoute}/:id`, authMiddleware, IdProductValidation, productController.findProductByID)
   .put(`${urlBaseRoute}/:id`, authMiddleware, putProductValidation, productController.updateProduct)
   .patch(`${urlBaseRoute}/:id`, authMiddleware, patchProductValidation, productController.updateProduct)
   .delete(`${urlBaseRoute}/:id`, authMiddleware, IdProductValidation, productController.deleteProductByID)
+  .get(`${urlBaseRoute}/low_stock`, authMiddleware, productController.findProductLowStock)
+  .get(`${urlBaseRoute}/marketplace/:id`, authMiddleware, IdProductValidation, productController.mapperProductByID)
+  .post(`${urlBaseRoute}/csv`, authMiddleware, multerConfig.single('file'), productController.createProductCSV)
 ;
 export default router;
