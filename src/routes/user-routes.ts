157e5a6d4b1f40043fa88fda3025a96e9b14057f
BrunoDelani <br.delani@gmail.com>
2022-09-05 10:28:20 -0400

    fix: remove authentication middleare in user routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -2,15 +2,14 @@ import { Router } from 'express';
 import userController from '../api/controllers/user-controller';
 import userValidation from '../api/validations/user/user-validation';
 import idUserValidation from '../api/validations/user/id-user-validation';
-import authMiddleware from '../middlewares/authenticate-user';
 
 const router = Router();
 const urlBaseRoute = '/api/v1';
 router
-  .get(`${urlBaseRoute}/user`, authMiddleware, userController.findUser)
+  .get(`${urlBaseRoute}/user`, userController.findUser)
   .post(`${urlBaseRoute}/user`, userValidation, userController.createUser)
-  .post(`${urlBaseRoute}/authenticate`, userValidation, userController.authenticateUser)
-  .delete(`${urlBaseRoute}/user/:id`, authMiddleware, idUserValidation, userController.deleteUser)
+  .post(`${urlBaseRoute}/authenticate`, userController.authenticateUser)
+  .delete(`${urlBaseRoute}/user/:id`, idUserValidation, userController.deleteUser)
 ;
 
 export default router;
