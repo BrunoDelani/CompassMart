a9cc87c883400a77140e78eb6f80540309d2b46f
BrunoDelani <br.delani@gmail.com>
2022-09-06 13:38:07 -0400

    feat: TDD DELETE/user

--- __tests__/integration/user.test.ts
+++ __tests__/integration/user.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from "../../src/app";
+var faker = require("faker");
 
 let server = request(app);
 jest.setTimeout(60000);
@@ -14,13 +15,12 @@ describe('GET /user', () => {
 });
 
 describe('POST /user', () => {
-    
     test('should register the user and return the same with JWT token', async () => {
         const response = await
             server
                 .post('/api/v1/user')
                 .send({
-                    "email": "user_test@compasso.com.br",
+                    "email": faker.internet.email(),
                     "password": "123456"
                 })
         expect(response.statusCode).toBe(201);
@@ -32,7 +32,7 @@ describe('POST /user', () => {
             server
                 .post('/api/v1/user')
                 .send({
-                    "email": "user_testcompasso-com-br",
+                    "email": faker.name.findName(),
                     "password": ""
                 })
         expect(response.statusCode).toBe(400);
@@ -49,5 +49,35 @@ describe('POST /user', () => {
                 })
         expect(response.statusCode).toBe(400);
         expect(response._body.token).toBeFalsy();
+    });
+});
+
+describe('DELETE /user', () => {
+    test('should delete the user with valid credentials', async () => {
+        const user = await
+            server
+                .post('/api/v1/user')
+                .send({
+                    "email": faker.internet.email(),
+                    "password": "123456"
+                })
+        const response = await
+            server
+                .delete(`/api/v1/user/${ user._body.id }`)
+        expect(response.statusCode).toBe(204);
+    });
+
+    test('should not proceed with invalid id', async () => {
+        const response = await
+            server
+                .delete(`/api/v1/user/invalidId`)
+        expect(response.statusCode).toBe(400);
+    });
+
+    test('should not found the user', async () => {
+        const response = await
+            server
+                .delete(`/api/v1/user/00000000a00000000a00a000`)
+        expect(response.statusCode).toBe(404);
     });
 });
