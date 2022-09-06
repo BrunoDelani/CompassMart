9911d2be33852b0d1b16fba83f2f7f5d4d71c9b2
BrunoDelani <br.delani@gmail.com>
2022-09-06 13:15:09 -0400

    feat: TDD POST/user

--- __tests__/integration/user.test.ts
+++ __tests__/integration/user.test.ts
@@ -12,3 +12,42 @@ describe('GET /user', () => {
         expect(response.statusCode).toBe(200);
     });
 });
+
+describe('POST /user', () => {
+    
+    test('should register the user and return the same with JWT token', async () => {
+        const response = await
+            server
+                .post('/api/v1/user')
+                .send({
+                    "email": "user_test@compasso.com.br",
+                    "password": "123456"
+                })
+        expect(response.statusCode).toBe(201);
+        expect(response._body.token).toBeDefined();
+    });
+
+    test('should not register the user with invalid credentials', async () => {
+        const response = await
+            server
+                .post('/api/v1/user')
+                .send({
+                    "email": "user_testcompasso-com-br",
+                    "password": ""
+                })
+        expect(response.statusCode).toBe(400);
+        expect(response._body.token).toBeFalsy();
+    });
+
+    test('should not register if email is already in use', async () => {
+        const response = await
+            server
+                .post('/api/v1/user')
+                .send({
+                    "email": "user_test@compasso.com.br",
+                    "password": "123456"
+                })
+        expect(response.statusCode).toBe(400);
+        expect(response._body.token).toBeFalsy();
+    });
+});
