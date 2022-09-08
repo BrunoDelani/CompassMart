60b89f4f249acebd768e2556818db9a03644178d
BrunoDelani <br.delani@gmail.com>
2022-09-08 13:44:29 -0400

    feat: TDD GET /product/low_stock

--- __tests__/integration/product.test.ts
+++ __tests__/integration/product.test.ts
@@ -18,6 +18,15 @@ const ProductExample = {
     "qtd_stock": 99999,
     "bar_codes": "1112223334445"
 }
+const ProductExampleLowStock = {
+    "title": "Azeites, Óleos e Vinagres",
+    "description": "Óleo de Soja Soya Garrafa 900ml",
+    "department": "Mercearia",
+    "brand": "Soya",
+    "price": 99.99,
+    "qtd_stock": 10,
+    "bar_codes": "5111222333444"
+}
 const PageNotFound = {
     page: -1,
     limit: -1
@@ -47,6 +56,34 @@ describe('GET /product', () => {
     test('should return page not found with invalid limit of results' , async () => {
         const login = await server.post('/api/v1/authenticate').send(UserToLogin)
         const response = await server.get(`/api/v1/product/?limit=${PageNotFound.limit}`).set('Authorization', `Bearer ${login._body.token}`)
+        expect(response.statusCode).toBe(404);
+    });
+});
+
+describe('GET /product/low_stock', () => {
+    test('should return all products with low stock', async () => {
+        const login = await server.post('/api/v1/authenticate').send(UserToLogin)
+        const createProduct = await server.post('/api/v1/product').send(ProductExampleLowStock).set('Authorization', `Bearer ${login._body.token}`)
+        const response = await server.get('/api/v1/product/low_stock').set('Authorization', `Bearer ${login._body.token}`)
+        await server.delete(`/api/v1/product/${ createProduct._body.id }`).set('Authorization', `Bearer ${login._body.token}`)
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('should return not products found with low stock' , async () => {
+        const login = await server.post('/api/v1/authenticate').send(UserToLogin)
+        const response = await server.get('/api/v1/product/low_stock').set('Authorization', `Bearer ${login._body.token}`)
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('should return page not found with invalid page on find products in low stock' , async () => {
+        const login = await server.post('/api/v1/authenticate').send(UserToLogin)
+        const response = await server.get(`/api/v1/product/low_stock/?page=${PageNotFound.page}`).set('Authorization', `Bearer ${login._body.token}`)
+        expect(response.statusCode).toBe(404);
+    });
+
+    test('should return page not found with invalid limit of results on find products in low stock' , async () => {
+        const login = await server.post('/api/v1/authenticate').send(UserToLogin)
+        const response = await server.get(`/api/v1/product/low_stock/?limit=${PageNotFound.limit}`).set('Authorization', `Bearer ${login._body.token}`)
         expect(response.statusCode).toBe(404);
     });
 });
