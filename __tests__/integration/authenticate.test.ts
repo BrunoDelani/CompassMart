406189e2c57d4620e97f1b65f3eccf468ae7bbb7
BrunoDelani <br.delani@gmail.com>
2022-09-09 11:17:24 -0400

    refactor: lint test files

--- __tests__/integration/authenticate.test.ts
+++ __tests__/integration/authenticate.test.ts
@@ -1,43 +1,43 @@
 import request from 'supertest';
-import app from "../../src/app";
+import app from '../../src/app';
 
-let server = request(app);
+const server = request(app);
 jest.setTimeout(60000);
 
 describe('POST /authentication', () => {
-    test('should receive JWT token when authenticated with valid credentials', async () => {
-        const response = await
-            server
-                .post('/api/v1/authenticate')
-                .send({
-                    "email": "bruno.teste@compasso.com.br",
-                    "password": "123456"
-                })
-        expect(response.statusCode).toBe(200);
-        expect(response._body.token).toBeDefined();
-    });
+  test('should receive JWT token when authenticated with valid credentials', async () => {
+    const response = await
+    server
+      .post('/api/v1/authenticate')
+      .send({
+        email: 'bruno.teste@compasso.com.br',
+        password: '123456'
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response._body.token).toBeDefined();
+  });
 
-    test('should not authenticate with invalid credentials', async () => {
-        const response = await
-            server
-                .post('/api/v1/authenticate')
-                .send({
-                    "email": "bruno.teste@compasso.com.br",
-                    "password": "123"
-                })
-        expect(response.statusCode).toBe(401);
-        expect(response._body.token).toBeFalsy();
-    });
+  test('should not authenticate with invalid credentials', async () => {
+    const response = await
+    server
+      .post('/api/v1/authenticate')
+      .send({
+        email: 'bruno.teste@compasso.com.br',
+        password: '123'
+      });
+    expect(response.statusCode).toBe(401);
+    expect(response._body.token).toBeFalsy();
+  });
 
-    test('should not authenticate with user not exists', async () => {
-        const response = await
-            server
-                .post('/api/v1/authenticate')
-                .send({
-                    "email": "not_exists@compasso.com.br",
-                    "password": "123"
-                })
-        expect(response.statusCode).toBe(404);
-        expect(response._body.token).toBeFalsy();
-    });
+  test('should not authenticate with user not exists', async () => {
+    const response = await
+    server
+      .post('/api/v1/authenticate')
+      .send({
+        email: 'not_exists@compasso.com.br',
+        password: '123'
+      });
+    expect(response.statusCode).toBe(404);
+    expect(response._body.token).toBeFalsy();
+  });
 });
